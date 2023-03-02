1b29db1deb43b6dd921c7edc19c9f7367c722dcc
rcynd <rcynd@cengr-c1251-118.engineering.missouri.edu>
2023-03-02 17:50:01 -0600

    e

--- main.c
+++ main.c
@@ -1,6 +1,187 @@
 #include <stdio.h>
+#include <string.h>
+
+void parseFromFile(FILE* fp,char* inst,int* reg1){
+    char temp[4];
+    fscanf(fp,"%s",inst);
+    fscanf(fp,"%s",temp);
+    if (temp[3]=='\0'){
+        *reg1=(int)temp[1]-48;
+    }
+    else{
+        *reg1=(int)temp[1]-48;
+        *reg1*=10;
+        *reg1+=(int)temp[2]-48;
+    }
+
+}
+
+void parseRemainder(FILE* fp,char* inst,int* reg2,int* reg3,int* imm){
+    char temp[7];
+    int count=1;
+    int j=11;
+    int neg=0;
+    int i=0;
+    *reg3=0;
+    fscanf(fp,"%s",temp);
+    if(strcmp(inst,"add")==0){
+        if (temp[3]=='\0'){
+            *reg2=(int)temp[1]-48;
+        }
+        else{
+            *reg2=(int)temp[1]-48;
+            *reg2*=10;
+            *reg2+=(int)temp[2]-48;
+        }
+        
+        fscanf(fp,"%s",temp);
+        if (temp[3]=='\0'){
+            *reg3=(int)temp[1]-48;
+        }
+        else{
+            *reg3=(int)temp[1]-48;
+            *reg3*=10;
+            *reg3+=(int)temp[2]-48;
+        }
+    }
+    if(strcmp(inst,"addi")==0){
+        if (temp[3]=='\0'){
+            *reg2=(int)temp[1]-48;
+        }
+        else{
+            *reg2=(int)temp[1]-48;
+            *reg2*=10;
+            *reg2+=(int)temp[2]-48;
+        }
+        
+        fscanf(fp,"%s",temp);
+        int i=0;
+        while(temp[i]!='\0'){
+            if((int)temp[i]==45){
+                neg=-1;
+            }
+            else{
+                j=(int)temp[i]-48;
+                if((i+neg)>0){
+                    *reg3*=10;
+                }
+                 *reg3+=j;
+            }
+            i++;
+        }
+        if(neg==-1){
+            *reg3*=neg;
+        }
+    }
+    if(strcmp(inst,"bge")==0){
+        if (temp[3]=='\0'){
+            *reg2=(int)temp[1]-48;
+        }
+        else{
+            *reg2=(int)temp[1]-48;
+            *reg2*=10;
+            *reg2+=(int)temp[2]-48;
+        }
+    }
+    if(strcmp(inst,"sll")==0){
+        if (temp[3]=='\0'){
+            *reg2=(int)temp[1]-48;
+        }
+        else{
+            *reg2=(int)temp[1]-48;
+            *reg2*=10;
+            *reg2+=(int)temp[2]-48;
+        }
+        
+        fscanf(fp,"%s",temp);
+        
+        while(temp[i]!='\0'){
+            if((int)temp[i]==45){
+                neg=-1;
+            }   
+            else{
+                j=(int)temp[i]-48;
+                if((i+neg)>0){
+                    *reg3*=10;
+                }
+                 *reg3+=j;
+            }
+            i++;
+        }
+        if(neg==-1){
+            *reg3*=neg;
+        }
+    }
+    if(strcmp(inst,"lw")==0){
+        while(temp[i]!='('){
+            if((int)temp[i]==45){
+                neg=-1;
+            }
+            else{
+                j=(int)temp[i]-48;
+            }
+            *imm+=j;
+        }
+        i++;
+    }
+        if(neg==-1){
+            *imm*=neg;
+        }
+        if (temp[i+3]=='\0'){
+            *reg2=(int)temp[i+1]-48;
+        }
+        else{
+            *reg2=(int)temp[i+1]-48;
+            *reg2*=10;
+            *reg2+=(int)temp[i+2]-48;
+        }
+
+}
+char *codeToHex(char* output,int rs1,int rs2,int rd,int immx){
+    int binary[32],rdx[5];
+    switch (output){
+        case "add":
+            binary[0]=1;
+            binary[1]=1;
+            binary[2]=0;
+            binary[3]=0;
+            binary[4]=1;
+            binary[5]=1;
+            binary[6]=0;
+            rd=decimalToBinaryReg(rd);
+            binary[7]=rdx[0];
+            binary[8]=rdx[1];
+            binary[9]=rdx[2];
+            binary[10]=rdx[3];
+            binary[11]=rdx[4];
+        break;
+
+    }
+}
+int* decimalToBinaryReg(int num) {   
+    if (num == 0) {
+        printf("0");
+        return;
+    }
+   int binaryNum[5]; // Assuming 32 bit integer.
+   int i=0;
+   for ( ;num > 0; ){
+      binaryNum[i++] = num % 2;
+      num /= 2;
+   }
+   return binaryNum;
+}
 
 int main(){
-    fopen("text.txt",r);
-    fscanf("%S\n");
-}
+    FILE* fp;
+    fp=fopen("text.txt","r");
+    //parseInstructionFromFile(fp);
+    char output[6];
+    int reg1,reg2,reg3,imm;
+    int *reg1x=&reg1,*reg2x=&reg2,*reg3x=&reg3,*immx=&imm;
+    
+    parseFromFile(fp,output,reg1x);
+    parseRemainder(fp,output,reg2x,reg3x,immx);
+    printf("%s %d %d %d\n",output,reg1,reg2,reg3);
+    return 0;
+}zero,
