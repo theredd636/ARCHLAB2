31424edb6001660e79c1ee88933daaf713bdf2fa
rcynd <rcynd@cengr-c1251-118.engineering.missouri.edu>
2023-03-09 17:53:24 -0600

    save

--- main.c
+++ main.c
@@ -182,6 +182,7 @@ void codeToHex(char* output,int rs1,int rs2,int rd,int immx,char hex[]){
     for(int j=0;j<32;j++){
         printf("%d",binary[31 - j]);
     }
+    puts("");
     binaryToHex(binary,hex);
     return hex;
 }
@@ -200,7 +201,10 @@ void binaryToHex(int binary[],char hex[]){
     temp[2] = 0;
     temp[3] = 0;
     int skip = 0;
-
+    for(int will=0;will<32;will++){
+        printf("%d",binary[31-will]);
+    }
+    puts("");
     for(int i = 0; i < 8 ; i++)
     {
         skip = (i * 4);
@@ -209,67 +213,67 @@ void binaryToHex(int binary[],char hex[]){
         binary[29 - skip] = temp[1];
         binary[28 - skip] = temp[0];
 
-        if((temp[0] == 0) & (temp[1] == 0) & (temp[2] == 0 ) & (temp[3] == 0))
+        if((temp[0] == 0) && (temp[1] == 0) && (temp[2] == 0 ) && (temp[3] == 0))
         {
-            hex[7- i] = 0;
+            hex[7- i] = '0';
         }
-        if((temp[0] == 1) & (temp[1] == 0) & (temp[2] == 0 ) & (temp[3] == 0))
+        if((temp[0] == 1) && (temp[1] == 0) && (temp[2] == 0 ) &&(temp[3] == 0))
         {
-            hex[7- i] = 1;
+            hex[7- i] = '1';
         }
-        if((temp[0] == 0) & (temp[1] == 1) & (temp[2] == 0 ) & (temp[3] == 0))
+        if((temp[0] == 0) && (temp[1] == 1) && (temp[2] == 0 ) && (temp[3] == 0))
         {
-            hex[7- i] = 2;
+            hex[7- i] = '2';
         }
-        if((temp[0] == 1) & (temp[1] == 1) & (temp[2] == 0 ) & (temp[3] == 0))
+        if((temp[0] == 1) && (temp[1] == 1) && (temp[2] == 0 ) && (temp[3] == 0))
         {
-            hex[7- i] = 3;
+            hex[7- i] = '3';
         }
-        if((temp[0] == 0) & (temp[1] == 0) & (temp[2] == 1 ) & (temp[3] == 0))
+        if((temp[0] == 0) && (temp[1] == 0) && (temp[2] == 1 ) && (temp[3] == 0))
         {
-            hex[7- i] = 4;
+            hex[7- i] = '4';
         }
-        if((temp[0] == 1) & (temp[1] == 0) & (temp[2] == 1 ) & (temp[3] == 0))
+        if((temp[0] == 1) && (temp[1] == 0) && (temp[2] == 1 ) && (temp[3] == 0))
         {
-            hex[7- i] = 5;
+            hex[7- i] = '5';
         }
-        if((temp[0] == 0) & (temp[1] == 1) & (temp[2] == 1 ) & (temp[3] == 0))
+        if((temp[0] == 0) && (temp[1] == 1) && (temp[2] == 1 ) && (temp[3] == 0))
         {
-            hex[7- i] = 6;
+            hex[7- i] = '6';
         }
-        if((temp[0] == 1) & (temp[1] == 1) & (temp[2] == 1 ) & (temp[3] == 0))
+        if((temp[0] == 1) && (temp[1] == 1) && (temp[2] == 1 ) && (temp[3] == 0))
         {
-            hex[7- i] = 7;
+            hex[7- i] = '7';
         }
-        if((temp[0] == 0) & (temp[1] == 0) & (temp[2] == 0 ) & (temp[3] == 1))
+        if((temp[0] == 0) && (temp[1] == 0) && (temp[2] == 0 ) && (temp[3] == 1))
         {
-            hex[7- i] = 8;
+            hex[7- i] = '8';
         }
-        if((temp[0] == 1) & (temp[1] == 0) & (temp[2] == 0 ) & (temp[3] == 1))
+        if((temp[0] == 1) && (temp[1] == 0) && (temp[2] == 0 ) && (temp[3] == 1))
         {
-            hex[7- i] = 9;
+            hex[7- i] = '9';
         }
-        if((temp[0] == 0) & (temp[1] == 1) & (temp[2] == 0 ) & (temp[3] == 1))
+        if((temp[0] == 0) && (temp[1] == 1) && (temp[2] == 0 ) && (temp[3] == 1))
         {
             hex[7- i] = 'a';
         }
-        if((temp[0] == 1) & (temp[1] == 1) & (temp[2] == 0 ) & (temp[3] == 1))
+        if((temp[0] == 1) && (temp[1] == 1) && (temp[2] == 0 ) && (temp[3] == 1))
         {
             hex[7- i] = 'b';
         }
-        if((temp[0] == 0) & (temp[1] == 0) & (temp[2] == 1 ) & (temp[3] == 1))
+        if((temp[0] == 0) && (temp[1] == 0) && (temp[2] == 1 ) && (temp[3] == 1))
         {
             hex[7- i] = 'c';
         }
-        if((temp[0] == 1) & (temp[1] == 0) & (temp[2] == 1 ) & (temp[3] == 1))
+        if((temp[0] == 1) && (temp[1] == 0) && (temp[2] == 1 ) && (temp[3] == 1))
         {
             hex[7- i] = 'd';
         }
-        if((temp[0] == 0) & (temp[1] == 1) & (temp[2] == 1 ) & (temp[3] == 1))
+        if((temp[0] == 0) && (temp[1] == 1) && (temp[2] == 1 ) && (temp[3] == 1))
         {
             hex[7- i] = 'e';
         }
-        if((temp[0] == 1) & (temp[1] == 1) & (temp[2] == 1 ) & (temp[3] == 1))
+        if((temp[0] == 1) && (temp[1] == 1) && (temp[2] == 1 ) && (temp[3] == 1))
         {
             hex[7- i] = 'f';
         }
